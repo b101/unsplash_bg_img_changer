60bbe16c81a6a14a241a1d5d509d127eb0c38a10
Branimir Rašić <branimir@Branimirs-iMac.local>
2019-04-22 02:16:10 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# daily random background image for macos using images from unsplash.com  
+## prerequisites
+1. you should register for free with unsplash.com to get an api_key.   
+2. you must create a folder where your downloded image will be stored   
+3. you open your system pref. > desktop and screen saver, choose custom location (folder) and add the folder you have just created, where your image will be stored  
+4. you check *change picture: * and change it to **daily**  
+
+## how to  
+if you checked all the prerequisites you will have to adjust the shell skript.   
---
+++ get_and_set_backgroundimage.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+#path to the location you want to store the downloaded image in
+save_to='/Users/YOURUSERNAME/Pictures/unsplash_bkgimages/'
+#the file name which the image will be stored under (obviously)
+filename='bkg_image.jpg'
+
+#unsplash api_key
+api_key='client_id=YOURAPIKEY'
+#unsplash width & height value for image
+width='5120'
+height='2880'
+#if your looking for images containing a certain keyword
+query='Croatia'
+
+#api url you are about to call using the defined unsplash settings
+url="https://api.unsplash.com/photos/random?$api_key&w=$width&h=$height&query=$query"
+#if you dont have jq installed, you can install it via brew install jq
+image=$(curl -s $url | jq --raw-output '.urls.custom')
+download="$save_to$filename"
+curl -s -o $download $image
